c89e3c5b5086d9f289aa096d1adb814e5bed8fa2
FURKAN DUMAN <160310062@ogr.cbu.edu.tr>
2021-10-06 14:25:30 +0300

    Update

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -19,7 +19,7 @@ namespace Business.Concrete
         {
             _carDal = carDal;
         }
-
+        
         public IResult Add(Car car)
         {
             if(car.DailyPrice > 0)
---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,23 @@
+﻿using Entitiy.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(p => p.Name).MinimumLength(2);
+            RuleFor(p => p.Name).NotEmpty();
+            RuleFor(p => p.DailyPrice).NotEmpty();
+            RuleFor(p => p.DailyPrice).GreaterThan(0);
+            
+
+        } 
+    }
+}
